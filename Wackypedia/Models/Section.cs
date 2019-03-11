8976b551b34d20cbc11c61b2f33455213c0feb9e
Guest User <Guest@staff-2.local>
2019-03-11 14:09:23 -0700

    add Section class code.

--- Wackypedia/Models/Section.cs
+++ Wackypedia/Models/Section.cs
@@ -9,18 +9,21 @@ namespace Wackypedia.Models
     private string MyTitle;
     private string MyImageLink;
     private string MyBody;
+    private int MyArticleID;
     private int MyID;
 
-    public Section(string title, string imageLink, string body, int ID = 0){
+    public Section(string title, string imageLink, string body, int articleID, int ID = 0){
       MyTitle = title;
       MyImageLink = imageLink;
       MyBody = body;
+      MyArticleID = articleID;
       MyID = ID;
     }
 
     public string GetTitle(){ return MyTitle; }
     public string GetImageLink(){ return MyImageLink; }
     public string GetBody(){ return MyBody; }
+    public int GetArticleID() { return ArticleID; }
     public int GetID(){ return MyID; }
 
     public static List<Section> GetAll(){
@@ -36,8 +39,9 @@ namespace Wackypedia.Models
         string title = rdr.GetString(1);
         string imageLink = rdr.GetString(2);
         string body = rdr.GetString(3); //This should be a TEXT field (MEDIUMTEXT/LONGTEXT)
-        Client newClient = new Client(firstName, lastName, phoneNumber, email, stylistID, clientID);
-        allClients.Add(newClient);
+        int articleID = rdr.GetInt32(4);
+        Section newSection = new Section(title, imageLink, body, articleID, sectionID);
+        allSections.Add(newSection);
       }
 
       conn.Close();
@@ -46,19 +50,18 @@ namespace Wackypedia.Models
         conn.Dispose();
       }
 
-      return allClients;
+      return allSections;
     }
 
-    public void SetFirstName(string firstName){ MyFirstName = firstName; }
-    public void SetLastName(string lastName){ MyLastName = lastName; }
-    public void SetPhoneNumber(string phoneNumber){ MyPhoneNumber = phoneNumber; }
-    public void SetEmail(string email){ MyEmail = email; }
-    public void SetStylistID(int stylistID){ MyStylistID = stylistID; }
+    public void SetTitle(string title){ MyTitle = title; }
+    public void SetImageLink(string imageLink){ MyImageLink = imageLink; }
+    public void SetBody(string body){ MyBody = body; }
+    public void SetArticleID(int articleID){ MyArticleID = articleID; }
     public static void ClearAll(){
       MySqlConnection conn = DB.Connection();
       conn.Open();
       var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"DELETE FROM clients;";
+      cmd.CommandText = @"DELETE FROM sections;";
       cmd.ExecuteNonQuery();
       conn.Close();
       if (conn != null)
@@ -71,27 +74,23 @@ namespace Wackypedia.Models
       MySqlConnection conn = DB.Connection();
       conn.Open();
       var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"INSERT INTO clients (firstName, lastName, phoneNumber, email, stylistID) VALUES (@firstName, @lastName, @phoneNumber, @email, @stylistID);";
-      MySqlParameter firstName = new MySqlParameter();
-      firstName.ParameterName = "@firstName";
-      firstName.Value = this.MyFirstName;
-      cmd.Parameters.Add(firstName);
-      MySqlParameter lastName = new MySqlParameter();
-      lastName.ParameterName = "@lastName";
-      lastName.Value = this.MyLastName;
-      cmd.Parameters.Add(lastName);
-      MySqlParameter phoneNumber = new MySqlParameter();
-      phoneNumber.ParameterName = "@phoneNumber";
-      phoneNumber.Value = this.MyPhoneNumber;
-      cmd.Parameters.Add(phoneNumber);
-      MySqlParameter email = new MySqlParameter();
-      email.ParameterName = "@email";
-      email.Value = this.MyEmail;
-      cmd.Parameters.Add(email);
-      MySqlParameter stylistID = new MySqlParameter();
-      stylistID.ParameterName = "@stylistID";
-      stylistID.Value = this.MyStylistID;
-      cmd.Parameters.Add(stylistID);
+      cmd.CommandText = @"INSERT INTO sections (title, imageLink, body, articleID) VALUES (@title, @imageLink, @body, @articleID);";
+      MySqlParameter title = new MySqlParameter();
+      title.ParameterName = "@title";
+      title.Value = this.MyTitle;
+      cmd.Parameters.Add(title);
+      MySqlParameter imageLink = new MySqlParameter();
+      imageLink.ParameterName = "@imageLink";
+      imageLink.Value = this.MyImageLink;
+      cmd.Parameters.Add(imageLink);
+      MySqlParameter body = new MySqlParameter();
+      body.ParameterName = "@body";
+      body.Value = this.MyBody;
+      cmd.Parameters.Add(body);
+      MySqlParameter articleID = new MySqlParameter();
+      articleID.ParameterName = "@articleID";
+      articleID.Value = this.MyArticleID;
+      cmd.Parameters.Add(articleID);
       cmd.ExecuteNonQuery();
       MyID = (int) cmd.LastInsertedId;
 
@@ -102,36 +101,31 @@ namespace Wackypedia.Models
       }
     }
 
-    public void Edit(string newFirstName, string newLastName, string newPhoneNumber, string newEmail){
+    public void Edit(string newTitle, string newImageLink, string newBody){
       MySqlConnection conn = DB.Connection();
       conn.Open();
       var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"UPDATE clients SET firstName = @firstName, lastName = @lastName, phoneNumber = @phoneNumber, email = @email WHERE ID = @searchId;";
-      MySqlParameter searchId = new MySqlParameter();
-      searchId.ParameterName = "@searchId";
-      searchId.Value = MyID;
-      cmd.Parameters.Add(searchId);
-      MySqlParameter firstName = new MySqlParameter();
-      firstName.ParameterName = "@firstName";
-      firstName.Value = newFirstName;
-      cmd.Parameters.Add(firstName);
-      MySqlParameter lastName = new MySqlParameter();
-      lastName.ParameterName = "@lastName";
-      lastName.Value = newLastName;
-      cmd.Parameters.Add(lastName);
-      MySqlParameter phoneNumber = new MySqlParameter();
-      phoneNumber.ParameterName = "@phoneNumber";
-      phoneNumber.Value = newPhoneNumber;
-      cmd.Parameters.Add(phoneNumber);
-      MySqlParameter email = new MySqlParameter();
-      email.ParameterName = "@email";
-      email.Value = newEmail;
-      cmd.Parameters.Add(email);
+      cmd.CommandText = @"UPDATE sections SET title = @title, imageLink = @imageLink, body = @body WHERE ID = @searchID;";
+      MySqlParameter searchID = new MySqlParameter();
+      searchID.ParameterName = "@searchID";
+      searchID.Value = MyID;
+      cmd.Parameters.Add(searchID);
+      MySqlParameter title = new MySqlParameter();
+      title.ParameterName = "@title";
+      title.Value = newTitle;
+      cmd.Parameters.Add(title);
+      MySqlParameter imageLink = new MySqlParameter();
+      imageLink.ParameterName = "@imageLink";
+      imageLink.Value = newImageLink;
+      cmd.Parameters.Add(imageLink);
+      MySqlParameter body = new MySqlParameter();
+      body.ParameterName = "@body";
+      body.Value = newBody;
+      cmd.Parameters.Add(body);
       cmd.ExecuteNonQuery();
-      MyFirstName = newFirstName;
-      MyLastName = newLastName;
-      MyPhoneNumber = newPhoneNumber;
-      MyEmail = newEmail;
+      MyTitle = newTitle;
+      MyImageLink = newImageLink;
+      MyBody = newBody;
       conn.Close();
       if (conn != null)
       {
@@ -139,52 +133,50 @@ namespace Wackypedia.Models
       }
     }
 
-    public override bool Equals(System.Object otherItem){
-      if (!(otherItem is Client))
+    public override bool Equals(System.Object otherSection){
+      if (!(otherSection is Section))
       {
         return false;
       }
       else
       {
-        Client newClient = (Client) otherItem;
-        bool clientEquality = (this.GetFirstName() == newClient.GetFirstName() && this.GetLastName() == newClient.GetLastName() && this.GetPhoneNumber() == newClient.GetPhoneNumber() && this.GetEmail() == newClient.GetEmail() && this.GetID() == newClient.GetID()  && this.GetStylistID() == newClient.GetStylistID());
-        return (clientEquality);
+        Section newSection = (Section) otherSection;
+        bool sectionEquality = (this.GetTitle() == newSection.GetTitle() && this.GetImageLink() == newSection.GetImageLink() && this.GetBody() == newSection.GetBody() && this.GetArticleID() == newSection.GetArticleID() && this.GetID() == newSection.GetID());
+        return (sectionEquality);
       }
     }
 
-    public static Client Find(int id){
+    public static Section Find(int ID){
       MySqlConnection conn = DB.Connection();
       conn.Open();
       var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"SELECT * FROM clients WHERE ID = @thisID;";
+      cmd.CommandText = @"SELECT * FROM sections WHERE ID = @thisID;";
       MySqlParameter thisID = new MySqlParameter();
       thisID.ParameterName = "@thisID";
-      thisID.Value = id;
+      thisID.Value = ID;
       cmd.Parameters.Add(thisID);
       var rdr = cmd.ExecuteReader() as MySqlDataReader;
-      int clientID = 0;
-      string firstName = "";
-      string lastName = "";
-      string phoneNumber = "";
-      string email = "";
-      int stylistID = 0;
+      int sectionID = 0;
+      string title = "";
+      string imageLink = "";
+      string body = "";
+      int articleID = 0;
       while (rdr.Read())
       {
-        clientID = rdr.GetInt32(0);
-        firstName = rdr.GetString(1);
-        lastName = rdr.GetString(2);
-        phoneNumber = rdr.GetString(3);
-        email = rdr.GetString(4);
-        stylistID = rdr.GetInt32(5);
+        sectionID = rdr.GetInt32(0);
+        title = rdr.GetString(1);
+        imageLink = rdr.GetString(2);
+        body = rdr.GetString(3); //This should be a TEXT field (MEDIUMTEXT/LONGTEXT)
+        articleID = rdr.GetInt32(4);
       }
-      Client foundClient = new Client(firstName, lastName, phoneNumber, email, stylistID, clientID);
+      Section newSection = new Section(title, imageLink, body, articleID, sectionID);
 
       conn.Close();
       if (conn != null)
       {
         conn.Dispose();
       }
-      return foundClient;
+      return newSection;
     }
 
   }

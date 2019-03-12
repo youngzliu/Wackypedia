345dae37b1c10a511abc0bfa532ea327a66a01a8
ts <tristansetha@gmail.com>
2019-03-12 14:08:13 -0700

    article controller update

--- Wackypedia/Controllers/ArticlesController.cs
+++ Wackypedia/Controllers/ArticlesController.cs
@@ -4,40 +4,53 @@ using Wackypedia.Models;
 
 namespace Wackypedia.Controllers
 {
-    public class ArticlesController : Controller
-    {
-        // article home page
-        [HttpGet("/articles")] 
-        public ActionResult Index()
-        {
-            List<Article> allArticles = Article.GetAll();
-            return View(allArticles);
-        }
+	public class ArticlesController : Controller
+	{
+		// article home page
+		[HttpGet("/articles")] 
+		public ActionResult Index()
+		{
+			List<Article> allArticles = Article.GetAll();
+			return View(allArticles);
+		}
 
-        [HttpGet("/articles/new")]
-        public ActionResult New()
-        {
-            return View();
-        }
+		[HttpGet("/articles/new")]
+		public ActionResult New()
+		{
+			return View();
+		}
 
-
-        [HttpPost("/articles/search")]
-        public ActionResult Search(string searchString)
-        {
-            // Get articles matching search string.  Place model in the
-            // 'Index' view.
-            List<Article> matchingArticles = new List<Article>();
-            matchingArticles.Add(new Article("Guess Who?!!"));
-            return View("Index", matchingArticles);
-        }
-
-        [HttpGet("/articles/{id}")]
-        public ActionResult ShowArticle(int id)
-        {
-            Article matchingArticle = new Article("Guess Who?!!");
-            return View("Article", matchingArticle);
-        }
+		[HttpPost("/articles")]
+		public ActionResult Create(string articleDescription)
+		{
+			Article newArticle = new Article(articleDescription);
+			newArticle.Save();
+			List<Article> allArticles = Article.GetAll();
+			return View("Index", allArticles);
+		}
 
 
-    }
+		[HttpPost("/articles/search")]
+		public ActionResult Search(string searchString)
+		{
+			// Get articles matching search string.  Place model in the
+			// 'Index' view.
+			List<Article> matchingArticles = new List<Article>();
+			matchingArticles.Add(new Article("Guess Who?!!"));
+			return View("Index", matchingArticles);
+		}
+
+		[HttpGet("/articles/{id}")]
+		public ActionResult Show(int id)
+		{
+			Dictionary<string, object> model = new Dictionary<string, object>();
+			Article selectedArticle = Article.Find(id);
+			List<Section> articleSections = selectedArticle.GetSections();
+			List<Author> articleAuthors = selectedArticle.GetAuthor();
+			model.Add("stylist", selectedArticle);
+			model.Add("sections", articleSections)
+			model.Add("authors", articleAuthors);
+			return View("Index", model);
+		}
+	}
 }

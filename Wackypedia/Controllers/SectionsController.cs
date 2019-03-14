f35dce4c82707a2fbdd016353d4fb7ccbea920e0
Young Liu <youngliu92@gmail.com>
2019-03-14 03:56:45 -0700

    add the ability to edit sections.

--- Wackypedia/Controllers/SectionsController.cs
+++ Wackypedia/Controllers/SectionsController.cs
@@ -23,5 +23,27 @@ namespace Wackypedia.Controllers{
       return RedirectToAction("Show", "Articles", new {articleID = artID});
     }
 
+    [HttpGet("/articles/{articleID}/sections/{sectionID}/edit")]
+    public ActionResult Edit(int articleID, int sectionID)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Article article = Article.Find(articleID);
+      model.Add("article", article);
+      Section section = Section.Find(sectionID);
+      model.Add("section", section);
+      return View(model);
+    }
+
+    [HttpPost("/articles/{artID}/sections/{sectionID}")]
+    public ActionResult Update(int artID, int sectionID, string title, string imageLink, string body, string author)
+    {
+      Section section = Section.Find(sectionID);
+      section.Edit(title, imageLink, body);
+      Author au = new Author(author);
+      au.Save();
+      au.AddArticle(Article.Find(artID));
+      return RedirectToAction("Show", "Articles", new {articleID = artID});
+    }
+
   }
 }
